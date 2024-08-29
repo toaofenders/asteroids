3a4494881998fc075098bb4edf1df826e1ba76ed
Calvsie <toaofenders@gmail.com>
2024-08-29 06:24:45 -0500

    Triangle man

--- main.py
+++ main.py
@@ -3,12 +3,17 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
 
     while True:
         for event in pygame.event.get():
@@ -16,8 +21,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        dt= clock.tick(60) / 1000
 
+        dt= clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
